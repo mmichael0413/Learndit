dc6c09bff3d9bdd702adff11525e55b33976a71d
Mike Michaelevich <mmichael0413@gmail.com>
2012-05-29 22:17:11 -0400

    add views for codes

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,2 +1,78 @@
 class CodesController < ApplicationController
+
+  def index
+    @codes = Code.all
+    
+    respond_to do |format|
+      format.html #index.html.erb
+      format.xml { render :xml => @codes }
+    end
+  end
+
+  def show
+    @code = Code.find(params[:id])
+  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @code }
+    end
+  end
+
+  def new
+    @code = Code.new
+  
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @code }
+    end
+  end
+
+  def edit
+    @code = Code.find(params[:id])
+  end
+
+  def create
+    @code = Code.new(params[:code])
+    
+    respond_to do |format|
+      if @code.save
+       
+        format.html { redirect_to(@code,
+                      :notice => 'Code snippet was successfully created.') }
+        format.xml  { render :xml => @code,
+                      :status => :created, :location => @code }
+      else
+        format.html { render :action => "New" }
+        format.xml  { render :xml => @code.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @code = Code.find(params[:id])
+  
+    respond_to do |format|
+      if @code.update_attributes(params[:code])
+        format.html { redirect_to(@code, 
+                      :notice => 'Code snippet was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "Edit" }
+        format.xml  { render :xml => @code.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @code = Code.find(params[:id])
+    @code.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to(codes_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,29 @@
 class TagsController < ApplicationController
+  def create
+    @tag = Tag.new(params[:tag])
+    
+    respond_to do |format|
+      if @tag.save
+       
+        format.html { redirect_to(@tag,
+                      :notice => 'Tag was successfully created.') }
+        format.xml  { render :xml => @tag,
+                      :status => :created, :location => @tag }
+      else
+        format.html { render :action => "New" }
+        format.xml  { render :xml => @tag.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to(codes_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
