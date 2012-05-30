7d45b332cc38dc9667d7b9ebad05cff74b912a4d
Mike Michaelevich <mmichael0413@gmail.com>
2012-05-29 20:53:55 -0400

    add it all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learndit::Application.config.secret_token = '85141bbe61d5905f137cf6a3cc257105c9ff1b0c42719f78e48d17bf6f0109e578b2db538823fe0edc0ab3f1802b2c58e97ed54a89d0a234ecaf578d9c78e27f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Learndit::Application.config.session_store :cookie_store, key: '_Learndit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Learndit::Application.config.session_store :active_record_store
