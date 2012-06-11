fdb840a7458d5050cfb167541d2a9a9cd7dfd495
Mike Michaelevich <mmichael0413@gmail.com>
2012-06-11 17:58:20 -0400

    add coderay and RedCloth

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 gem 'pg'
+gem 'coderay'
+gem 'RedCloth'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  #def coderay(text)  
+  #    text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do  
+  #      CodeRay.scan($3, $2).div(:css => :class)  
+  #    end  
+  #  end
+    
+  def coderay(text) 
+      text.gsub!(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m) do 
+        code = CodeRay.scan($2, $1).div(:css => :class) 
+        "<notextile>#{code}</notextile>" 
+      end 
+      return text.html_safe 
+    end
 end
